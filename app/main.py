5b006a5f5227f0d11333e4dc76b0cfa5917e43a2
dielbatista <dielxlem229@gmail.com>
2026-03-05 17:32:59 -0300

    setting up routes.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.routes import router as usuario_router
+from app.database import Base, engine
 
 app = FastAPI()
+app.include_router(usuario_router)
 
 @app.get("/")
 def root():
---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app import crud
+
+router = APIRouter()
+
+@router.get("/usuarios/{usuario_id}")
+def read_usuario(usuario_id: int, db: Session = Depends(get_db)):
+    db_usuario = crud.get_usuario_by_id(db, usuario_id)
+    if db_usuario is None:
+        raise HTTPException(status_code=404, detail="Usuario not found")
+    return db_usuario
+
+@router.delete("/usuarios/{usuario_id}")
+def delete_usuario(usuario_id: int, db: Session = Depends(get_db)):
+    db_usuario = crud.get_usuario_by_id(db, usuario_id)
+    if db_usuario is None:
+        raise HTTPException(status_code=404, detail="Usuario not found")
+    crud.delete_usuario(db, usuario_id)
+    return {"message": "Usuario deleted successfully"}
